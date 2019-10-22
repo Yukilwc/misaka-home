96b563f7bde47e4deebbfe85471182cc4a72bead
Yukilwc <misaka1033@gmail.com>
2019-10-23 00:06:58 +0800

    插入

--- app/controller/test.js
+++ app/controller/test.js
@@ -6,7 +6,10 @@ const Controller = require('egg').Controller;
 class TestController extends Controller {
   async index() {
     const { ctx } = this;
-    sqlUtil.connectDB()
+    // let res = await sqlUtil.select('select * from [test].[dbo].[user]')
+    // let res = await sqlUtil.select("insert into [test].[dbo].[user](id,name,level,role,add_time) values ('00000001','liwenchao','1','admin',getdate())")
+    // console.log('test res', res)
+    await sqlUtil.insert()
     ctx.body = 'hi, egg';
   }
 }
--- app/sqlutil/index.js
+++ app/sqlutil/index.js
@@ -1,26 +1,38 @@
-// import sql from 'mssql'
 const sql = require('mssql')
 const se = require('../../secret/index')
+// 全局使用的连接池
+function poolError(err) {
+    console.log('---------------------------mssql connect error', err)
+}
+const pool = new sql.ConnectionPool(se.mssqlConfig, poolError)
+pool.close()
+/**
+ * 建立一次连接校验config
+ */
+pool.connect((err) => {
+    console.log('pool connect err', err)
+})
 
-
-const connectDB = async () => {
-    // console.log('----------------------start connect', config)
-    let res
-    try{
-    let pool = await sql.connect(se.mssqlConfig)
-    // await sql.connect('mssql://test:test123@localhost/test')
-    
-    res = await pool.request().query('select * from [test].[dbo].[Product]')
-    // res = await sql.query('select * from Product')
-
+const select = async (sqlStr) => {
+    try {
+        res = await pool.request().query(sqlStr)
+        console.log('query completed')
+        return res
     }
-    catch(e) {
-        console.log('----------------------------connect error', e)
+    catch (e) {
+        console.log('----------------------------query error', e)
+        // throw new  Error(e)       
+        return Promise.reject(e)
     }
 
-    console.log('-------------------------------res is', res)
 }
 
-module.exports =  {
-     connectDB,
+const insert = async (sqlStr, params) => {
+ let res =   pool.request().input('add_time', sql.DateTime, new Date())
+ console.log()
+}
+
+module.exports = {
+    select,
+    insert,
 }
